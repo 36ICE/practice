64fa6aea5b8c7468f14db8cffcf6fbc507716266
zengcd <635727914@qq.com>
2022-11-05 18:20:51 +0800

    HJ刷题  求int型正整数在内存中存储时1的个数

---
+++ mycoder/mynowcoder/src/main/java/hw/code/H15.java
@@ -0,0 +1,31 @@
+package hw.code;
+
+import java.util.Scanner;
+
+
+/**
+ * 求int型正整数在内存中存储时1的个数
+ * Integer.toBinaryString(num);
+ * Integer.toOctalString(num);
+ * Integer.toHexString(num);
+ */
+public class H15 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        int num = in.nextInt();
+        String s = Integer.toBinaryString(num);
+//        Integer.toOctalString(num);
+//        Integer.toHexString(num);
+        int c = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                c++;
+            }
+
+        }
+        System.out.println(c);
+    }
+}
