417411dc432f3212b0f69f69782f7157a41f547e
zengcd <635727914@qq.com>
2022-11-05 17:26:14 +0800

    HJ刷题 BigDecimal使用，精度问题

--- mycoder/mynowcoder/src/main/java/hw/code/H6.java
+++ mycoder/mynowcoder/src/main/java/hw/code/H6.java
@@ -1,9 +1,80 @@
 package hw.code;
 
+import java.util.Scanner;
+
+
+/**
+ * 质数的性质
+ * （1）质数p的约数只有两个：1和p。
+ * （2）初等数学基本定理：任一大于1的自然数，要么本身是质数，要么可以分解为几个质数之积，且这种分解是唯一的。
+ * （3）质数的个数是无限的。
+ * （4）大于10的质数中，个位数只有1,3,7,9。
+ *
+ * 如果N (N>=2)没有小于等于根号N，大于1的约数，那么N必然是质数。
+ * 同时可以使用 i*i<=N来简化 i<=sqrt(N)
+ */
 public class H6 {
 
 
     public static void main(String[] args) {
+
+        /**
+         * int 的大小是 4 个字节。它可以存储从 -2,147,483,648 到 2,147,483,647 的值
+         * long 8 字节的原始数据类型,它可以存储从 -9,223,372,036,854,775,808 到 9,223,372,036,854,775,807 的值。
+         */
+
+
+        t1();
+    }
+
+    /**
+     * 注意
+     *
+     */
+    public static void t1() {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        long k = (long) Math.sqrt(num);// 优化,使得for循环提早结束.
+
+        //如果N (N>=2)没有小于等于根号N，大于1的约数，那么N必然是质数。
+        for (long i = 2; i <= k; i++) {
+            while (num != i) {
+                if (num % i == 0) {
+                    System.out.print(i + " ");
+                    num = num / i;
+                } else {
+                    break;
+                }
+
+            }
+        }
+        System.out.print(num);
+    }
+
+    /**
+     * 我的错误解法
+     *
+     */
+    public static void t2() {
+        Scanner in = new Scanner(System.in);
+        long num = in.nextLong();
+        long k = (long) Math.sqrt(num);
+
+        //1、是<=k还是 <k,边界不清晰
+        for (int i = 2; i < k; i++) {
+            //2、注意此处与下面t1解法的区别，一个是可以>=  一个是!=
+            //在这个数本身是质数的时候会失败 64577
+            while (num >= i) {
+                if (num % i == 0) {
+                    System.out.print(i + " ");
+                    num /= i;
+                } else {
+                    break;
+                }
+            }
+
+
+        }
 
     }
 }
---
+++ mycoder/mynowcoder/src/main/java/hw/code/H7.java
@@ -0,0 +1,46 @@
+package hw.code;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+
+/**
+ * https://www.runoob.com/w3cnote/java-the-different-float-double.html
+ * double
+ * float
+ * 比如float类型是32位，是单精度浮点表示法：
+ * 符号位（sign）占用1位，用来表示正负数。
+ * 指数位（exponent）占用 8 位，用来表示指数。
+ * 小数位（fraction）占用 23 位，用来表示小数，不足位数补 0。
+ * 而 double 类型是 64 位，是双精度浮点表示法：
+ * 符号位占用 1 位，指数位占用 11 位，小数位占用 52 位。
+ * 到这里其实已经可以隐隐看出：
+ * 指数位决定了大小范围，因为指数位能表示的数越大则能表示的数越大嘛！
+ * 而小数位决定了计算精度，因为小数位能表示的数越大，则能计算的精度越大咯！
+ *可能还不够明白，举例子吧：
+ * float 的小数位只有 23 位，即二进制的 23 位，能表示的最大的十进制数为 2 的 23 次方，即 8388608，即十进制的 7 位，
+ * 严格点，精度只能百分百保证十进制的 6 位运算。
+ * double 的小数位有 52 位，对应十进制最大值为 4 503 599 627 370 496，这个数有 16 位，所以计算精度只能百分百保证十进制的 15 位运算。
+ *
+ * PS: 我们常见的科学计算器，比如高中时候用的，一般最大支持的运算位数就是 15 位，超过这个就不够准了。在实际编程中，也是用的 double 类型比较多，
+ * 因为能够保证 15 位的运算。如果还需要更高精度的运算，则需要使用其他数据类型，比如 java 中的 BigDecimal 类型，能够支持更高精度的运算。
+ *
+ *指数位的偏移量与无符号表示
+ * 需要注意的是指数可能是负数，也有可能是正数，即指数是有符号整数，而有符号整数的计算是比无符号整数麻烦的。所以为了减少不必要的麻烦，
+ * 在实际存储指数的时候，需要把指数转换成无符号整数。那么怎么转换呢？
+ *
+ * 注意到 float 的指数部分是 8 位，则指数的取值范围是 -126 到 +127，为了消除负数带来的实际计算上的影响（比如比较大小，加减法等），
+ * 可以在实际存储的时候，给指数做一个简单的映射，加上一个偏移量，比如float的指数偏移量为 127，这样就不会有负数出现了。
+ */
+public class H7 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        String  v = in.next();
+        BigDecimal bigDecimal=new BigDecimal(v);
+        bigDecimal = bigDecimal.setScale(0, BigDecimal.ROUND_HALF_UP);
+        System.out.println(bigDecimal);
+    }
+}
